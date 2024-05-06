ed1b3471025617c5b762bd4fb0fb6a938af4fdad
Geonwoo Pack <packdev937@gmail.com>
2024-05-06 19:58:07 +0900

    chore : deploy.sh 내 docker-compose.yml 경로를 수정한다

--- deploy.sh
+++ deploy.sh
@@ -41,19 +41,19 @@ else
 fi
 
 echo "$TARGET_SERVICE Deploy..."
-docker-compose -f /home/docker-compose.yml up -d $TARGET_SERVICE $BATCH_CONTAINER
+docker-compose -f /home/ec2-user/docker-compose.yml up -d $TARGET_SERVICE $BATCH_CONTAINER
 
 # Wait for the target service to be healthy before proceeding
 sleep 10
 
 if [ -z "$RUNNING_NGINX" ]; then
     echo "Starting Nginx..."
-    docker-compose -f /home/docker-compose.yml up -d nginx
+    docker-compose -f /home/ec2-user/docker-compose.yml up -d nginx
 fi
 
 # Update the nginx config and reload
 sed -i "s/$OTHER_SERVICE/$TARGET_SERVICE/" $NGINX_CONF
-docker-compose -f /home/docker-compose.yml restart nginx
+docker-compose -f /home/ec2-user/docker-compose.yml restart nginx
 
 # Stop the other service
-docker-compose -f /home/docker-compose.yml stop $OTHER_SERVICE
+docker-compose -f /home/ec2-user/docker-compose.yml stop $OTHER_SERVICE
