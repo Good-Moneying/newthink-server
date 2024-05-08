eb33de81d3694ca9a4d94f0e888c27be15fe2976
Geonwoo Pack <packdev937@gmail.com>
2024-05-08 22:38:56 +0900

    #1 test : 뉴스 레터 생성을 테스트 한다 - 에디터 권한이 없다면 예외를 발생한다

--- application/src/test/java/kusitms/duduk/application/newsletter/service/CreateNewsLetterCommandTest.java
+++ application/src/test/java/kusitms/duduk/application/newsletter/service/CreateNewsLetterCommandTest.java
@@ -1,11 +1,22 @@
 package kusitms.duduk.application.newsletter.service;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import kusitms.duduk.core.newsletter.dto.NewsLetterDtoMapper;
-import kusitms.duduk.core.newsletter.dto.request.CreateAiNewsLetterRequest;
+import java.time.LocalDate;
+import kusitms.duduk.core.newsletter.dto.request.CreateNewsLetterRequest;
+import kusitms.duduk.core.newsletter.dto.response.NewsLetterResponse;
 import kusitms.duduk.core.newsletter.port.input.CreateNewsLetterUseCase;
-import kusitms.duduk.domain.newsletter.NewsLetter;
+import kusitms.duduk.core.user.port.output.SaveUserPort;
+import kusitms.duduk.domain.user.User;
+import kusitms.duduk.domain.user.vo.Acorn;
+import kusitms.duduk.domain.user.vo.Email;
+import kusitms.duduk.domain.user.vo.Gender;
+import kusitms.duduk.domain.user.vo.Goal;
+import kusitms.duduk.domain.user.vo.Nickname;
+import kusitms.duduk.domain.user.vo.Provider;
+import kusitms.duduk.domain.user.vo.RefreshToken;
+import kusitms.duduk.domain.user.vo.Role;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,21 +29,76 @@ public class CreateNewsLetterCommandTest {
     @Autowired
     private CreateNewsLetterUseCase createNewsLetterCommand;
 
+    @Autowired
+    private SaveUserPort saveUserPort;
+
     @Test
-    void AI_뉴스_레터를_생성한다() {
+    void 뉴스_레터를_생성한다() {
         // given
-        CreateAiNewsLetterRequest request = 뉴스_레터_생성_요청();
+        CreateNewsLetterRequest request = 뉴스_레터_생성_요청();
+        String email = ROLE_EDITOR_생성_요청();
 
         // when
-        NewsLetter newsLetter = createNewsLetterCommand.create(request);
+        NewsLetterResponse response = createNewsLetterCommand.create(request, email);
 
         // then
-        assertThat(newsLetter).isNotNull();
-        assertThat(newsLetter.getSummary().getSentences().size()).isEqualTo(3);
-        assertThat(newsLetter.getSummary().getSentences().get(0)).isEqualTo("물가 상승률이 예상보다 높다.");
+        assertThat(response).isNotNull();
+        assertThat(response.title()).isEqualTo(request.title());
+        assertThat(response.content()).isEqualTo(request.content());
     }
 
-    private static CreateAiNewsLetterRequest 뉴스_레터_생성_요청() {
+    @Test
+    void 편집_권한이_없으면_뉴스_레터를_생성할_수_없다() {
+        // given
+        CreateNewsLetterRequest request = 뉴스_레터_생성_요청();
+        String email = ROLE_USER_생성_요청();
+
+        // when & then
+        assertThatThrownBy(() -> createNewsLetterCommand.create(request, email))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private String ROLE_EDITOR_생성_요청() {
+        String email = "test@test.com";
+
+        User user = User.builder()
+            .email(Email.of(email))
+            .nickname(Nickname.of("tester"))
+            .refreshToken(RefreshToken.of("12345"))
+            .goal(Goal.EVERYDAY)
+            .acorn(Acorn.initial())
+            .gender(Gender.MALE)
+            .birthday(LocalDate.of(1990, 1, 1))
+            .provider(Provider.KAKAO)
+            .role(Role.EDITOR)
+            .build();
+
+        saveUserPort.save(user);
+
+        return email;
+    }
+
+    private String ROLE_USER_생성_요청() {
+        String email = "test1@test.com";
+
+        User user = User.builder()
+            .email(Email.of(email))
+            .nickname(Nickname.of("tester1"))
+            .refreshToken(RefreshToken.of("123456"))
+            .goal(Goal.EVERYDAY)
+            .acorn(Acorn.initial())
+            .gender(Gender.MALE)
+            .birthday(LocalDate.of(1990, 1, 1))
+            .provider(Provider.KAKAO)
+            .role(Role.USER)
+            .build();
+
+        saveUserPort.save(user);
+
+        return email;
+    }
+
+    private static CreateNewsLetterRequest 뉴스_레터_생성_요청() {
         String thumbnail = "https://image.com/tesla.jpg";
         String title = "테슬라 주가 갑자기 오르는 이유는?";
         String content = "불과 몇 주 사이에 미국 토화 정책을 바라보는 분위기가 완전히 바뀌었어요. 얼마 전까진 다들 '오해 6월에 금리 인하가 시작될 것'이라고 했다";
@@ -41,7 +107,7 @@ public class CreateNewsLetterCommandTest {
         String summary = "물가 상승률이 예상보다 높다.\n급격한 기준 금리 인상한다\n이민자 증가한다\n";
         final String aiType = "AI";
 
-        CreateAiNewsLetterRequest request = new CreateAiNewsLetterRequest(
+        CreateNewsLetterRequest request = new CreateNewsLetterRequest(
             thumbnail,
             title,
             content,
