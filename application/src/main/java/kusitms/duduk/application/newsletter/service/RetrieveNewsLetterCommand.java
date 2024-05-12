71caf62309d577ae0f6cdca4bcc153cdc57380b2
Geonwoo Pack <packdev937@gmail.com>
2024-05-12 19:04:07 +0900

    #11 feat : 오늘의 뉴스레터를 조회한다 - 가장 최신의 에디터 뉴스레터를 필터링 하여 조회한다

--- application/src/main/java/kusitms/duduk/application/newsletter/service/RetrieveNewsLetterCommand.java
+++ application/src/main/java/kusitms/duduk/application/newsletter/service/RetrieveNewsLetterCommand.java
@@ -3,9 +3,11 @@ package kusitms.duduk.application.newsletter.service;
 import kusitms.duduk.core.newsletter.dto.NewsLetterDtoMapper;
 import kusitms.duduk.core.newsletter.dto.request.RetrieveNewsLetterRequest;
 import kusitms.duduk.core.newsletter.dto.response.NewsLetterResponse;
+import kusitms.duduk.core.newsletter.dto.response.NewsLetterThumbnailResponse;
 import kusitms.duduk.core.newsletter.port.input.RetrieveNewsLetterQuery;
 import kusitms.duduk.core.newsletter.port.output.LoadNewsLetterPort;
 import kusitms.duduk.domain.newsletter.NewsLetter;
+import kusitms.duduk.domain.user.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -22,5 +24,13 @@ public class RetrieveNewsLetterCommand implements RetrieveNewsLetterQuery {
         NewsLetter newsLetter = loadNewsLetterPort.findById(request.id())
             .orElseThrow(() -> new IllegalArgumentException("해당 뉴스레터를 찾을 수 없습니다."));
         return newsLetterDtoMapper.toDto(newsLetter);
+    }
+
+    public NewsLetterThumbnailResponse retriveLatestNewsLetter(User user) {
+        NewsLetter newsLetter = loadNewsLetterPort.findLatestNewsLetter()
+            .orElseThrow(() -> new IllegalArgumentException("오늘의 뉴스레터를 찾을 수 없습니다."));
+
+        boolean isScrapped = user.isScrapped(newsLetter);
+        return newsLetterDtoMapper.toThumbnailDto(newsLetter, isScrapped);
     }
 }
--- core/src/main/java/kusitms/duduk/core/newsletter/port/input/RetrieveNewsLetterQuery.java
+++ core/src/main/java/kusitms/duduk/core/newsletter/port/input/RetrieveNewsLetterQuery.java
@@ -2,7 +2,10 @@ package kusitms.duduk.core.newsletter.port.input;
 
 import kusitms.duduk.core.newsletter.dto.request.RetrieveNewsLetterRequest;
 import kusitms.duduk.core.newsletter.dto.response.NewsLetterResponse;
+import kusitms.duduk.core.newsletter.dto.response.NewsLetterThumbnailResponse;
+import kusitms.duduk.domain.user.User;
 
 public interface RetrieveNewsLetterQuery {
     NewsLetterResponse retrieve(RetrieveNewsLetterRequest request);
+    NewsLetterThumbnailResponse retriveLatestNewsLetter(User user);
 }
