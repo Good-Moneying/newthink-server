4b553ed86f736b699f775df170bb841daae5b2d1
Kwon <seaweed8759@naver.com>
2024-05-18 21:17:00 +0900

    hotfix: batch server의 application.yml 파일 수정한다

--- batch/src/test/java/kusitms/duduk/batch/utils/ParsingAiContentUtilsTest.java
+++ batch/src/test/java/kusitms/duduk/batch/utils/ParsingAiContentUtilsTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -17,6 +18,7 @@ public class ParsingAiContentUtilsTest {
     private ParsingAiContent parsingAiContent;
 
     @Test
+    @Transactional
     public void 정상적인_입력시_결과를_잘_출력한다(){
 
         String message = "본문: 한국은행이 발표한 자료에 따르면 간편송금 이용금액이 하루 2000억원을 넘어섰다. 이용금액은 지난해 대비 60.7% 증가한 2005억원으로 나타났고, 이용건수도 34.8% 증가한 218만건이었다. 한은은 카카오페이, 토스 등이 서비스를 제공함에 따라 시장 경쟁이 치열해지면서 이용규모가 크게 증가했다고 분석했다. 마케팅 비용으로 1000억원 이상을 소비한 업체도 있었다. \n"
