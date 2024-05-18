7ccfde16fb552f469b84975ecb86e680a85a0759
Kwon <seaweed8759@naver.com>
2024-05-18 20:49:54 +0900

    hotfix: application-test.yml 파일을 추가한다

--- batch/src/test/java/kusitms/duduk/batch/utils/ParsingAiContentUtilsTest.java
+++ batch/src/test/java/kusitms/duduk/batch/utils/ParsingAiContentUtilsTest.java
@@ -27,7 +27,7 @@ public class ParsingAiContentUtilsTest {
         ParsedAiContentResponse parsedAiContent = parsingAiContent.getParsingResult(message);
 
         ParsedAiContentResponse parsedAiContent1 = ParsedAiContentResponse.builder()
-                .headline("하루 2000억' 판 커지는 간편송금 시장")
+                .title("하루 2000억' 판 커지는 간편송금 시장")
                 .content("한국은행이 발표한 자료에 따르면 간편송금 이용금액이 하루 2000억원을 넘어섰다. 이용금액은 지난해 대비 60.7% 증가한 2005억원으로 나타났고, 이용건수도 34.8% 증가한 218만건이었다. 한은은 카카오페이, 토스 등이 서비스를 제공함에 따라 시장 경쟁이 치열해지면서 이용규모가 크게 증가했다고 분석했다. 마케팅 비용으로 1000억원 이상을 소비한 업체도 있었다.")
                 .keywords("한국은행, 간편송금, 카카오페이")
                 .category("FINANCE")
